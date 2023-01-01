ba7ee91fad0226cb09064281aeced392a284b090
Oluwatoyin Ariyo <35049514+toyinariyo@users.noreply.github.com>
2023-01-01 19:30:49 +0000

    Create main.py

---
+++ Beginner/Day 1 Band Name Generator/main.py
@@ -0,0 +1,4 @@
+print("Hello user. Welcome to Toyin's band name generator")
+city = input("What city did you grow up?\n")
+pet = input("What is the name of your pet?\n")
+print("Here is your band name: " + city + " " + pet)
