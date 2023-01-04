e93bc4804101ccc60fa193d137ead0bee65ff90d
Oluwatoyin Ariyo <35049514+toyinariyo@users.noreply.github.com>
2023-01-04 19:11:40 +0000

    Removed one rounding line

--- Beginner/Day 2 Tip Calculator/main.py
+++ Beginner/Day 2 Tip Calculator/main.py
@@ -6,6 +6,5 @@ tip_percentage = tip / 100
 total_tip = bill * tip_percentage
 total_bill = bill + total_tip
 bill_per_person = total_bill / people_split
-final_amount = round(bill_per_person, 2)
 final_amount = "{:.2f}".format(bill_per_person)
 print(f"Each person should pay: ${final_amount}" )
