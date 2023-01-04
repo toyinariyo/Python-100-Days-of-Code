570eacf3155f2d55bd49c1cd78ed1e70a4e6103d
Oluwatoyin Ariyo <35049514+toyinariyo@users.noreply.github.com>
2023-01-04 18:35:01 +0000

    Added formatting to final amount
    So answer will show up with 2 decimal places e.g. 15.40 instead of 15.4

--- Beginner/Day 2 Tip Calculator/main.py
+++ Beginner/Day 2 Tip Calculator/main.py
@@ -7,4 +7,5 @@ total_tip = bill * tip_percentage
 total_bill = bill + total_tip
 bill_per_person = total_bill / people_split
 final_amount = round(bill_per_person, 2)
+final_amount = "{:.2f}".format(bill_per_person)
 print(f"Each person should pay: ${final_amount}" )
