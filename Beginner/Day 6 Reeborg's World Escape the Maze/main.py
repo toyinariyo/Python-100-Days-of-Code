5e76ff18ecaa96810b6ef73d4e128b98f7e4b667
Oluwatoyin Ariyo <35049514+toyinariyo@users.noreply.github.com>
2023-01-15 21:55:24 +0000

    Reeborg's World Escape the maze main.py

---
+++ Beginner/Day 6 Reeborg's World Escape the Maze/main.py
@@ -0,0 +1,17 @@
+def right_turn():
+    turn_left()
+    turn_left()
+    turn_left()
+ 
+while front_is_clear():
+    move()
+turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        right_turn()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
