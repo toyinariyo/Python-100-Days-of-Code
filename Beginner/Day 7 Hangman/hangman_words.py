caa36f98f28b466821db888fb7cae7277e5cbf64
Oluwatoyin Ariyo <35049514+toyinariyo@users.noreply.github.com>
2023-01-19 20:24:39 +0000

    Replaced Pythonista comment with Pythonic

--- Beginner/Day 7 Hangman/hangman_words.py
+++ Beginner/Day 7 Hangman/hangman_words.py
@@ -1,4 +1,4 @@
 print("Loading wordlist from file...")
 file_open = open("wordlist_file.txt", 'r')
-word_list = [line for line in file_open]  # Used list comprehension instead of for loop to make code more Pythonista.
+word_list = [line for line in file_open]  # Used list comprehension instead of for loop to make code more Pythonic.
 print(" ", len(word_list), " words found.")
