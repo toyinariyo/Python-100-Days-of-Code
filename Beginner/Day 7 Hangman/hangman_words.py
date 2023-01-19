2575ef4ba6f4e21ccf387bf0dca2e32695e6b154
Oluwatoyin Ariyo <35049514+toyinariyo@users.noreply.github.com>
2023-01-19 19:59:43 +0000

    Replaced for loop with list comprehension

--- Beginner/Day 7 Hangman/hangman_words.py
+++ Beginner/Day 7 Hangman/hangman_words.py
@@ -1,6 +1,4 @@
 print("Loading wordlist from file...")
 file_open = open("wordlist_file.txt", 'r')
-word_list = []
-for line in file_open:
-    word_list.append(line.strip().lower())
+word_list = [line for line in file_open]  # Used list comprehension instead of for loop to make code more Pythonista.
 print(" ", len(word_list), " words found.")
