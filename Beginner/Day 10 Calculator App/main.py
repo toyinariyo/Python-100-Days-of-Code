73f1e8ab5e13850040802608abdf269fecf00174
Oluwatoyin Ariyo <35049514+toyinariyo@users.noreply.github.com>
2023-01-28 12:13:56 +0000

    Make Calculator App more Pythonic
    By using operator module instead of four different functions for add, subtract, multiply and divide.

--- Beginner/Day 10 Calculator App/main.py
+++ Beginner/Day 10 Calculator App/main.py
@@ -1,4 +1,5 @@
 import os
+import operator
 from art import logo
 
 
@@ -6,49 +7,30 @@ def cls():  # Cross-platform clear screen
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-# Add
-def add(n1, n2):
-    return n1 + n2
-
-
-# Subtract
-def subtract(n1, n2):
-    return n1 - n2
-
-
-# Multiply
-def multiply(n1, n2):
-    return n1 * n2
-
-
-# Divide
-def divide(n1, n2):
-    return n1 / n2
-
-
 def calculator():
     print(logo)
 
 
 operations = {
-    "+": add,
-    "-": subtract,
-    "*": multiply,
-    "/": divide
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv
 }
 
 first_number = float(input("What's the first number? "))
 second_number = float(input("What's the second number? "))
 for sign in operations:
     print(sign)
-    should_continue = True
+should_continue = True
 
 while should_continue:
     operation_sign = input("Pick an operation from the line above: ")
     calculation_function = operations[operation_sign]
     answer = calculation_function(first_number, second_number)
     print(f"{first_number} {operation_sign} {second_number} = {answer}")
-    if input("Type 'y' to continue calculating with {answer} or type 'n' to start a new calculation: ") == "y":
+    yes_or_no = input("Type 'y' to continue calculating with {answer} or type 'n' to start a new calculation: ")
+    if yes_or_no.strip().lower() == "y":
         first_number = answer
     else:
         should_continue = False
